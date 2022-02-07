d2c5596e85e3fcd01834e39f09943cbcbc581aa4
Mohit Gupta <guptamm.22@gmail.com>
2022-02-07 15:20:07 +0530

    Pacgae for differnt Role

---
+++ app/src/main/java/cmsr/ipsacademy/net/Activities/Principal/PrincipalActivity.java
@@ -0,0 +1,16 @@
+package cmsr.ipsacademy.net.Activities.Principal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cmsr.ipsacademy.net.R;
+
+public class PrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
