4b581f381e412c709ae0691da5e9ffa9681f5ca5
anmol192 <anmolkushwah19@gmail.com>
2022-03-12 16:04:20 +0530

    tag:menu file

--- app/src/main/java/cmsr/ipsacademy/net/activities/Teacher/TeacherActivity.kt
+++ app/src/main/java/cmsr/ipsacademy/net/activities/Teacher/TeacherActivity.kt
@@ -9,7 +9,7 @@ import androidx.drawerlayout.widget.DrawerLayout
 import cmsr.ipsacademy.net.R
 import com.google.android.material.navigation.NavigationView
 
-class TeacherAc : AppCompatActivity() {
+class TeacherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_teacher)
@@ -26,61 +26,8 @@ class TeacherAc : AppCompatActivity() {
         drawerLayout.addDrawerListener(actionBarDrawerToggle)
         actionBarDrawerToggle.syncState()
 
-        navigationView.setNavigationItemSelectedListener(NavigationView.OnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.ClassCoordinator -> Toast.makeText(
-                    applicationContext,
-                    "Class Coordinator",
-                    Toast.LENGTH_SHORT
-                ).show()
-                R.id.MyStudent -> Toast.makeText(applicationContext, "MyStudent", Toast.LENGTH_SHORT)
-                    .show()
-                R.id.SubjectCoordinator -> Toast.makeText(
-                    applicationContext,
-                    "Subject Coordinator",
-                    Toast.LENGTH_SHORT
-                ).show()
-                R.id.Events -> Toast.makeText(
-                    applicationContext,
-                    "Events",
-                    Toast.LENGTH_SHORT
-                ).show()
-                R.id.Feedbacks -> Toast.makeText(
-                    applicationContext,
-                    "Feedbacks",
-                    Toast.LENGTH_SHORT
-                ).show()
-                R.id.NBA -> Toast.makeText(applicationContext, "NBA", Toast.LENGTH_SHORT).show()
-                R.id.UpdateStudentDetail -> Toast.makeText(applicationContext, "UpdateStudentDetail", Toast.LENGTH_SHORT)
-                    .show()
-                R.id.onlineexam -> Toast.makeText(
-                    applicationContext,
-                    "online exam",
-                    Toast.LENGTH_SHORT
-                ).show()
-                R.id.subjectCoordinator -> Toast.makeText(
-                    applicationContext,
-                    "subjectCoordinator",
-                    Toast.LENGTH_SHORT
-                ).show()
-                R.id.myStudent -> Toast.makeText(
-                    applicationContext,
-                    "myStudent",
-                    Toast.LENGTH_SHORT
-                ).show()
-                R.id.attendance -> Toast.makeText(
-                    applicationContext,
-                    "attendance",
-                    Toast.LENGTH_SHORT
-                ).show()
-                R.id.events-> Toast.makeText(
-                    applicationContext,
-                    "events",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-            true
-        })
+
+
 
     }
 }
--- app/src/main/java/cmsr/ipsacademy/net/activities/Teacher/teacher.kt
+++ app/src/main/java/cmsr/ipsacademy/net/activities/Teacher/teacher.kt
@@ -1,10 +1,12 @@
-package cmsr.ipsacademy.net
+package cmsr.ipsacademy.net.activities.Teacher
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import cmsr.ipsacademy.net.R
+import com.google.android.material.navigation.NavigationView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,7 +18,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [teacher.newInstance] factory method to
  * create an instance of this fragment.
  */
-class teacher : Fragment() {
+class teacher : Fragment(R.layout.fragment_teacher) {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,26 +36,14 @@ class teacher : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        val NavigationView: NavigationView = requireView().findViewById(R.id.navigation_menu)
+
+
         return inflater.inflate(R.layout.fragment_teacher, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment teacher.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            teacher().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
+
+
+
 }
