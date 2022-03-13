0b5b6e6652c6a66af1d09df964280c4420f25cec
anmol192 <anmolkushwah19@gmail.com>
2022-03-13 13:53:11 +0530

    tag:menu

--- app/src/main/java/cmsr/ipsacademy/net/activities/Teacher/TeacherActivity.kt
+++ app/src/main/java/cmsr/ipsacademy/net/activities/Teacher/TeacherActivity.kt
@@ -1,33 +1,12 @@
-package cmsr.ipsacademy.net.activities
+package cmsr.ipsacademy.net.activities.Teacher
 
-import android.os.Bundle
-import android.widget.Toast
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import androidx.drawerlayout.widget.DrawerLayout
+import android.os.Bundle
 import cmsr.ipsacademy.net.R
-import com.google.android.material.navigation.NavigationView
 
-class TeacherActivity : AppCompatActivity() {
+class HodActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_teacher)
-        val toolbar = findViewById<Toolbar>(R.id.toolBar)
-        setSupportActionBar(toolbar)
-
-        val navigationView = findViewById<NavigationView>(R.id.teacher_menu)
-        navigationView.setItemIconTintList(null)
-        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer)
-
-
-        val actionBarDrawerToggle =
-            ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close)
-        drawerLayout.addDrawerListener(actionBarDrawerToggle)
-        actionBarDrawerToggle.syncState()
-
-
-
-
     }
 }
