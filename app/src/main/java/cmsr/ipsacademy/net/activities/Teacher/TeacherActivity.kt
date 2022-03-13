a9d3590ef16404c907abad388f1ea54a98207817
anmol192 <anmolkushwah19@gmail.com>
2022-03-13 17:59:16 +0530

    tag:menu

--- app/src/main/java/cmsr/ipsacademy/net/activities/Teacher/TeacherActivity.kt
+++ app/src/main/java/cmsr/ipsacademy/net/activities/Teacher/TeacherActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import cmsr.ipsacademy.net.R
 
-class HodActivity : AppCompatActivity() {
+class TeacherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_teacher)
