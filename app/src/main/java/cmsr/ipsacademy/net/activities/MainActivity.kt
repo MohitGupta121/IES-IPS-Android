e445797eaded11541776b1565338bcc848706ecc
Mohit Gupta <guptamm.22@gmail.com>
2022-03-21 14:42:57 +0530

    layout fixed

--- app/src/main/java/cmsr/ipsacademy/net/activities/MainActivity.kt
+++ app/src/main/java/cmsr/ipsacademy/net/activities/MainActivity.kt
@@ -36,88 +36,88 @@ class MainActivity : AppCompatActivity() {
 //        }
 
 
-        val userIdList = resources.getStringArray(R.array.UsersId)
-
-        userIdSpinner = findViewById(R.id.choose_user)
-        if (userIdSpinner != null) {
-            val adapter = ArrayAdapter(
-                this,
-                android.R.layout.simple_spinner_item, userIdList
-            )
-            userIdSpinner!!.adapter = adapter
-
-            userIdSpinner!!.onItemSelectedListener = object :
-                AdapterView.OnItemSelectedListener {
-                override fun onItemSelected(
-                    parent: AdapterView<*>,
-                    view: View, position: Int, id: Long
-                ) {
-                    sharedPreference.save("role", userIdList.get(position))
-                    Toast.makeText(
-                        applicationContext,
-                        sharedPreference.getValueString("role"),
-                        Toast.LENGTH_SHORT
-                    )
-                        .show()
-                    sharedPreference.getValueString("role")?.let { Log.i("SharedTag", it) }
-                }
-
-                override fun onNothingSelected(parent: AdapterView<*>) {
-                    // write code to perform some action
-                }
-            }
-        }
-
-            inputEmail = findViewById(R.id.edit_email)
-            inputPassword = findViewById(R.id.edit_password)
-            loginButton = findViewById(R.id.button_login)
-
-            auth = FirebaseAuth.getInstance()
-
-            loginButton!!.setOnClickListener(View.OnClickListener {
-                val email = inputEmail!!.text.toString().trim()
-                val password = inputPassword!!.text.toString().trim()
-
-                if (TextUtils.isEmpty(email)) {
-                    Toast.makeText(
-                        applicationContext,
-                        "PLease Enter your E-Mail",
-                        Toast.LENGTH_SHORT
-                    )
-                        .show()
-                    return@OnClickListener
-                }
-                if (TextUtils.isEmpty(password)) {
-                    Toast.makeText(
-                        applicationContext,
-                        "PLease Enter your password",
-                        Toast.LENGTH_SHORT
-                    )
-                        .show()
-                    return@OnClickListener
-                }
-
-                auth!!.signInWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(this, OnCompleteListener { task ->
-                        if (!task.isSuccessful) {
-                            if (password.length < 6) {
-                                inputPassword!!.setError(getString(R.string.minimum_password))
-                            } else {
-                                Toast.makeText(
-                                    this,
-                                    getString(R.string.auth_failed),
-                                    Toast.LENGTH_LONG
-                                )
-                                    .show()
-                            }
-                            Log.e("login", "Login Error", task.exception)
-                        } else{
-                            startActivity(Intent(this@MainActivity,UserActivity::class.java))
-                            finish()
-                        }
-                    })
-
-            })
+//        val userIdList = resources.getStringArray(R.array.UsersId)
+//
+//        userIdSpinner = findViewById(R.id.choose_user)
+//        if (userIdSpinner != null) {
+//            val adapter = ArrayAdapter(
+//                this,
+//                android.R.layout.simple_spinner_item, userIdList
+//            )
+//            userIdSpinner!!.adapter = adapter
+//
+//            userIdSpinner!!.onItemSelectedListener = object :
+//                AdapterView.OnItemSelectedListener {
+//                override fun onItemSelected(
+//                    parent: AdapterView<*>,
+//                    view: View, position: Int, id: Long
+//                ) {
+//                    sharedPreference.save("role", userIdList.get(position))
+//                    Toast.makeText(
+//                        applicationContext,
+//                        sharedPreference.getValueString("role"),
+//                        Toast.LENGTH_SHORT
+//                    )
+//                        .show()
+//                    sharedPreference.getValueString("role")?.let { Log.i("SharedTag", it) }
+//                }
+//
+//                override fun onNothingSelected(parent: AdapterView<*>) {
+//                    // write code to perform some action
+//                }
+//            }
+//        }
+//
+//            inputEmail = findViewById(R.id.edit_email)
+//            inputPassword = findViewById(R.id.edit_password)
+//            loginButton = findViewById(R.id.button_login)
+//
+//            auth = FirebaseAuth.getInstance()
+//
+//            loginButton!!.setOnClickListener(View.OnClickListener {
+//                val email = inputEmail!!.text.toString().trim()
+//                val password = inputPassword!!.text.toString().trim()
+//
+//                if (TextUtils.isEmpty(email)) {
+//                    Toast.makeText(
+//                        applicationContext,
+//                        "PLease Enter your E-Mail",
+//                        Toast.LENGTH_SHORT
+//                    )
+//                        .show()
+//                    return@OnClickListener
+//                }
+//                if (TextUtils.isEmpty(password)) {
+//                    Toast.makeText(
+//                        applicationContext,
+//                        "PLease Enter your password",
+//                        Toast.LENGTH_SHORT
+//                    )
+//                        .show()
+//                    return@OnClickListener
+//                }
+//
+//                auth!!.signInWithEmailAndPassword(email, password)
+//                    .addOnCompleteListener(this, OnCompleteListener { task ->
+//                        if (!task.isSuccessful) {
+//                            if (password.length < 6) {
+//                                inputPassword!!.setError(getString(R.string.minimum_password))
+//                            } else {
+//                                Toast.makeText(
+//                                    this,
+//                                    getString(R.string.auth_failed),
+//                                    Toast.LENGTH_LONG
+//                                )
+//                                    .show()
+//                            }
+//                            Log.e("login", "Login Error", task.exception)
+//                        } else{
+//                            startActivity(Intent(this@MainActivity,UserActivity::class.java))
+//                            finish()
+//                        }
+//                    })
+//
+//            })
 
     }
 }
