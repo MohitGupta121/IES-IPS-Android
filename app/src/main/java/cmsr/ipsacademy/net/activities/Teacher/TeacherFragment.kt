9dc9c89a6f08e174ca5ac8d636d3822ae809bd50
Mohit Gupta <guptamm.22@gmail.com>
2022-04-05 12:47:38 +0530

    updated teacher fragment

--- app/src/main/java/cmsr/ipsacademy/net/activities/Teacher/TeacherFragment.kt
+++ app/src/main/java/cmsr/ipsacademy/net/activities/Teacher/TeacherFragment.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import cmsr.ipsacademy.net.R
 
-class TeacherActivity : AppCompatActivity() {
+class TeacherFragment : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_teacher)
+        setContentView(R.layout.fragment_teacher)
     }
 }
