02fcbe09e2d77405b1579c1a984eb2d4709b7f27
Mohit Gupta <guptamm.22@gmail.com>
2022-07-25 13:49:52 +0530

    minor changes

--- app/src/main/java/cmsr/ipsacademy/net/adapters/AttendancePanelViewAdapter.kt
+++ app/src/main/java/cmsr/ipsacademy/net/adapters/AttendancePanelViewAdapter.kt
@@ -55,6 +55,8 @@ class AttendancePanelViewAdapter(val context: Context): RecyclerView.Adapter<Att
                     val activity = it.context as AppCompatActivity
                     val bundle = Bundle()
                     bundle.putString("clg_sub_code", modal.clg_sub_code)
+                    bundle.putString("batch_id", modal.batch_id)
+                    bundle.putString("semester", modal.semester)
                     fragment.arguments = bundle
                     activity.supportFragmentManager.beginTransaction().replace(R.id.frame_layout_container, fragment).addToBackStack(null).commit()
                 }
